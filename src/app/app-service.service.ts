0b6cb5799862c9d05ab535a7ec97b33b555a8131
hodygmd <gmdhody@gmail.com>
2024-06-25 21:57:10 -0600

    add url backup and checking for each endpoint

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { catchError } from 'rxjs/operators';
 import {
   CategoriaInterface, DetalleVentaInterface, EmpleadoInterface,
   MrcFncnInterface,
@@ -13,7 +14,6 @@ import {AddCategoria} from "./Classes/add-categoria";
 import {AddPresentacion} from "./Classes/add-presentacion";
 import {AddUnidadMedida} from "./Classes/add-unidad-medida";
 import {AddPuesto} from "./Classes/add-puesto";
-import * as http from "http";
 import {AddEmpleado} from "./Classes/add-empleado";
 import {AddVenta} from "./Classes/add-venta";
 import {AddDetalleVenta} from "./Classes/add-detalle-venta";
@@ -22,35 +22,67 @@ import {AddDetalleVenta} from "./Classes/add-detalle-venta";
   providedIn: 'root'
 })
 export class AppServiceService {
-  private baseUrl = 'http://localhost:8080';
+  //private baseUrl = 'http://localhost:8080';
+  private baseUrlPrimary = 'http://localhost:8080';
+  private baseUrlSecondary = 'http://localhost:8081';
   private token = '?token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2NlIiwiY2xhdmUiOiJlbXBsMjMwMDg2NiIsImlhdCI6MTcxOTEyNjkzMiwiZXhwIjoxNzE5OTkwOTMyfQ.kJhZKJqROhLBylzs1EhYfmpeoQxzLShg7oIQwFAgQe0';
 
   constructor(private http: HttpClient) {
   }
+  /*-------------------------------------------------------------------------------------*/
+  /*----------------------------------METODO GENERICO------------------------------------*/
+
+  /*-------------------------------------------------------------------------------------*/
+  // Método para comprobar la URL principal y, si falla, intentar con la de respaldo
+  private checkUrls<T>(endpoint:string,method: string, body?: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url1=`${this.baseUrlPrimary}${endpoint}`
+    const url2=`${this.baseUrlSecondary}${endpoint}`
+    return this.http.request<T>(method, `${url1}${this.token}`, { headers, body }).pipe(
+      catchError((error) => {
+        console.error(`Error en la URL ${url1}. Intentando con la URL de respaldo...`);
+        // Si falla la URL principal, intenta con la URL de respaldo
+        return this.http.request<T>(method, `${url2}${this.token}`, { headers, body });
+      })
+    );
+  }
+  /*-------------------------------------------------------------------------------------*/
+  /*--------------------------------------AUTH-------------------------------------------*/
+
+  /*-------------------------------------------------------------------------------------*/
   auth(username: string, password: string) {
-    return this.http.post<EmpleadoInterface>(`${this.baseUrl}/empleado/login${this.token}`,
-      new AddEmpleado('','',username,password,0,1))
+    const endpoint = `/empleado/login`;
+    return this.checkUrls<EmpleadoInterface>(endpoint,'POST',
+      new AddEmpleado('','',username,password,0,1));
   }
   /*-------------------------------------------------------------------------------------*/
   /*------------------------------------PRODUCTOS----------------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getProductos() {
-    return this.http.get<ProductoInterface>(`${this.baseUrl}/producto${this.token}`)
+    const endpoint = `/producto`;
+    return this.checkUrls<ProductoInterface>(endpoint,'GET');
   }
 
-  createProducto(clv: string, nom: string, desc: string, pre: number, stmin: number, stmax: number, exis: number, idcat: number, idpres: number, idmarc: number) {
-    return this.http.post<ProductoInterface>(`${this.baseUrl}/producto/create${this.token}`,
-      new AddProducto(clv, nom, desc, pre, stmin, stmax, exis, idcat, idpres, idmarc, 1))
+  createProducto(clv: string, nom: string, desc: string, pre: number, stmin: number,
+                 stmax: number, exis: number, idcat: number, idpres: number,
+                 idmarc: number) {
+    const endpoint = `/producto/create`;
+    return this.checkUrls<ProductoInterface>(endpoint,'POST',
+      new AddProducto(clv, nom, desc, pre, stmin, stmax, exis, idcat, idpres, idmarc, 1));
   }
 
-  updateProducto(clv: string, nom: string, desc: string, pre: number, stmin: number, stmax: number, exis: number, idcat: number, idpres: number, idmarc: number) {
-    return this.http.put<ProductoInterface>(`${this.baseUrl}/producto/update/${clv}${this.token}`,
-      new AddProducto(clv, nom, desc, pre, stmin, stmax, exis, idcat, idpres, idmarc, 1))
+  updateProducto(clv: string, nom: string, desc: string, pre: number, stmin: number,
+                 stmax: number, exis: number, idcat: number, idpres: number,
+                 idmarc: number) {
+    const endpoint= `/producto/update/${clv}`;
+    return this.checkUrls<ProductoInterface>(endpoint,'PUT',
+      new AddProducto(clv, nom, desc, pre, stmin, stmax, exis, idcat, idpres, idmarc, 1));
   }
 
   deleteProducto(clave: string) {
-    return this.http.put<ProductoInterface>(`${this.baseUrl}/producto/delete/${clave}${this.token}`, '')
+    const endpoint=`/producto/delete/${clave}`
+    return this.checkUrls<ProductoInterface>(endpoint,'PUT','');
   }
 
   /*--------------------------------------------------------------------------------------*/
@@ -58,21 +90,25 @@ export class AppServiceService {
 
   /*--------------------------------------------------------------------------------------*/
   getCategorias() {
-    return this.http.get<CategoriaInterface>(`${this.baseUrl}/categoria${this.token}`)
+    const endpoint = `/categoria`;
+    return this.checkUrls<CategoriaInterface>(endpoint,'GET');
   }
 
   createCategoria(nom: string, idfun: number) {
-    return this.http.post<CategoriaInterface>(`${this.baseUrl}/categoria/create${this.token}`,
+    const endpoint=`/categoria/create`
+    return this.checkUrls<CategoriaInterface>(endpoint,'POST',
       new AddCategoria(nom, idfun, 1))
   }
 
   updateCategoria(id: number, nom: string, idfun: number) {
-    return this.http.put<CategoriaInterface>(`${this.baseUrl}/categoria/update/${id}${this.token}`,
+    const endpoint=`/categoria/update/${id}`
+    return this.checkUrls<CategoriaInterface>(endpoint,'PUT',
       new AddCategoria(nom, idfun, 1))
   }
 
   deleteCategoria(id: number) {
-    return this.http.put<CategoriaInterface>(`${this.baseUrl}/categoria/delete/${id}${this.token}`, '')
+    const endpoint=`/categoria/delete/${id}`
+    return this.checkUrls<CategoriaInterface>(endpoint,'PUT','')
   }
 
   /*------------------------------------------------------------------------------------------*/
@@ -80,21 +116,25 @@ export class AppServiceService {
 
   /*------------------------------------------------------------------------------------------*/
   getPresentaciones() {
-    return this.http.get<PresentacionInterface>(`${this.baseUrl}/presentacion${this.token}`)
+    const endpoint = `/presentacion`;
+    return this.checkUrls<PresentacionInterface>(endpoint,'GET');
   }
 
   createPresentacion(nom: string, desc: string, idunmed: number, med: number) {
-    return this.http.post<PresentacionInterface>(`${this.baseUrl}/presentacion/create${this.token}`,
+    const endpoint=`/presentacion/create`
+    return this.checkUrls<PresentacionInterface>(endpoint,'POST',
       new AddPresentacion(nom, desc, idunmed, med, 1))
   }
 
   updatePresentacion(id: number, nom: string, desc: string, idunmed: number, med: number) {
-    return this.http.put<PresentacionInterface>(`${this.baseUrl}/presentacion/update/${id}${this.token}`,
+    const endpoint=`/presentacion/update/${id}`
+    return this.checkUrls<PresentacionInterface>(endpoint,'PUT',
       new AddPresentacion(nom, desc, idunmed, med, 1))
   }
 
   deletePresentacion(id: number) {
-    return this.http.put<PresentacionInterface>(`${this.baseUrl}/presentacion/delete/${id}${this.token}`, '')
+    const endpoint=`/presentacion/delete/${id}`
+    return this.checkUrls<PresentacionInterface>(endpoint,'PUT','')
   }
 
   /*----------------------------------------------------------------------------------*/
@@ -102,21 +142,25 @@ export class AppServiceService {
 
   /*----------------------------------------------------------------------------------*/
   getMarcas() {
-    return this.http.get<MrcFncnInterface>(`${this.baseUrl}/marca${this.token}`)
+    const endpoint = `/marca`;
+    return this.checkUrls<MrcFncnInterface>(endpoint,'GET');
   }
 
   createMarca(nom: string, desc: string) {
-    return this.http.post<MrcFncnInterface>(`${this.baseUrl}/marca/create${this.token}`,
+    const endpoint=`/marca/create`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'POST',
       new AddMrcfncn(nom, desc, 1))
   }
 
   updateMarca(id: number, nom: string, desc: string) {
-    return this.http.put<MrcFncnInterface>(`${this.baseUrl}/marca/update/${id}${this.token}`,
+    const endpoint=`/marca/update/${id}`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'PUT',
       new AddMrcfncn(nom, desc, 1))
   }
 
   deleteMarca(id: number) {
-    return this.http.put<MrcFncnInterface>(`${this.baseUrl}/marca/delete/${id}${this.token}`, '')
+    const endpoint=`/marca/delete/${id}`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'PUT','')
   }
 
   /*-------------------------------------------------------------------------------------*/
@@ -124,105 +168,129 @@ export class AppServiceService {
 
   /*-------------------------------------------------------------------------------------*/
   getFunciones() {
-    return this.http.get<MrcFncnInterface>(`${this.baseUrl}/funcion${this.token}`)
+    const endpoint = `/funcion`;
+    return this.checkUrls<MrcFncnInterface>(endpoint,'GET');
   }
   createFuncion(nom: string, desc: string) {
-    return this.http.post<MrcFncnInterface>(`${this.baseUrl}/funcion/create${this.token}`,
+    const endpoint=`/funcion/create`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'POST',
       new AddMrcfncn(nom,desc,1))
   }
   updateFuncion(id:number,nom: string, desc: string){
-    return this.http.put<MrcFncnInterface>(`${this.baseUrl}/funcion/update/${id}${this.token}`,
+    const endpoint=`/funcion/update/${id}`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'PUT',
       new AddMrcfncn(nom,desc,1))
   }
   deleteFuncion(id:number){
-    return this.http.put<MrcFncnInterface>(`${this.baseUrl}/funcion/delete/${id}${this.token}`,'')
+    const endpoint=`/funcion/delete/${id}`
+    return this.checkUrls<MrcFncnInterface>(endpoint,'PUT','')
   }
   /*-------------------------------------------------------------------------------------*/
   /*--------------------------------UNIDADES DE MEDIDA-----------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getUnidadesMedida() {
-    return this.http.get<UnidadMedidaInterface>(`${this.baseUrl}/unidad-medida${this.token}`)
+    const endpoint = `/unidad-medida`;
+    return this.checkUrls<UnidadMedidaInterface>(endpoint,'GET');
   }
   createUnidadMedida(unidad:string){
-    return this.http.post<UnidadMedidaInterface>(`${this.baseUrl}/unidad-medida/create${this.token}`,
+    const endpoint=`/unidad-medida/create`
+    return this.checkUrls<UnidadMedidaInterface>(endpoint,'POST',
       new AddUnidadMedida(unidad,1))
   }
   updateUnidadMedida(id:number,unidad:string){
-    return this.http.put<UnidadMedidaInterface>(`${this.baseUrl}/unidad-medida/update/${id}${this.token}`,
+    const endpoint=`/unidad-medida/update/${id}`
+    return this.checkUrls<UnidadMedidaInterface>(endpoint,'PUT',
       new AddUnidadMedida(unidad,1))
   }
   deleteUnidadMedida(id:number){
-    return this.http.put<UnidadMedidaInterface>(`${this.baseUrl}/unidad-medida/delete/${id}${this.token}`,'')
+    const endpoint=`/unidad-medida/delete/${id}`
+    return this.checkUrls<UnidadMedidaInterface>(endpoint,'PUT','')
   }
   /*-------------------------------------------------------------------------------------*/
   /*-------------------------------------PUESTOS-----------------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getPuestos(){
-    return this.http.get<PuestoInterface>(`${this.baseUrl}/puesto${this.token}`)
+    const endpoint = `/puesto`;
+    return this.checkUrls<PuestoInterface>(endpoint,'GET');
   }
   createPuesto(nom:string){
-    return this.http.post<PuestoInterface>(`${this.baseUrl}/puesto/create${this.token}`,
+    const endpoint=`/puesto/create`
+    return this.checkUrls<PuestoInterface>(endpoint,'POST',
       new AddPuesto(nom,1))
   }
   updatePuesto(id:number,nom:string){
-    return this.http.put<PuestoInterface>(`${this.baseUrl}/puesto/update/${id}${this.token}`,
+    const endpoint=`/puesto/update/${id}`
+    return this.checkUrls<PuestoInterface>(endpoint,'PUT',
       new AddPuesto(nom,1))
   }
   deletePuesto(id:number){
-    return this.http.put<PuestoInterface>(`${this.baseUrl}/puesto/delete/${id}${this.token}`,'')
+    const endpoint=`/puesto/delete/${id}`
+    return this.checkUrls<PuestoInterface>(endpoint,'PUT','')
   }
   /*-------------------------------------------------------------------------------------*/
   /*------------------------------------EMPLEADOS----------------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getEmpleados(){
-    return this.http.get<EmpleadoInterface>(`${this.baseUrl}/empleado${this.token}`)
+    const endpoint = `/empleado`;
+    return this.checkUrls<EmpleadoInterface>(endpoint,'GET');
   }
   createEmpleado(clave:string,nom:string,username:string,password:string,idP:number){
-    return this.http.post<EmpleadoInterface>(`${this.baseUrl}/empleado/create${this.token}`,
+    const endpoint=`/empleado/create`
+    return this.checkUrls<EmpleadoInterface>(endpoint,'POST',
       new AddEmpleado(clave,nom,username,password,idP,1))
   }
   updateEmpleado(clave:string,nom:string,username:string,password:string,idP:number){
-    return this.http.put<EmpleadoInterface>(`${this.baseUrl}/empleado/update/${clave}${this.token}`,
+    const endpoint=`/empleado/update/${clave}`
+    return this.checkUrls<EmpleadoInterface>(endpoint,'PUT',
       new AddEmpleado(clave,nom,username,password,idP,1))
   }
   updatePassword(clave:string,nom:string,username:string,password:string,idP:number){
-    return this.http.put<EmpleadoInterface>(`${this.baseUrl}/empleado/update-pass/${clave}${this.token}`,
+    const endpoint=`/empleado/update-pass/${clave}`
+    return this.checkUrls<EmpleadoInterface>(endpoint,'PUT',
       new AddEmpleado(clave,nom,username,password,idP,1))
   }
   deleteEmpleado(clave:string){
-    return this.http.put<EmpleadoInterface>(`${this.baseUrl}/empleado/delete/${clave}${this.token}`,'')
+    const endpoint=`/empleado/delete/${clave}`
+    return this.checkUrls<EmpleadoInterface>(endpoint,'PUT','')
   }
   /*-------------------------------------------------------------------------------------*/
   /*----------------------------------DETALLES VENTAS------------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getProductosByFolio(folio:string){
-    return this.http.get<DetalleVentaInterface>(`${this.baseUrl}/dv/${folio}${this.token}`)
+    const endpoint = `/dv/${folio}`;
+    return this.checkUrls<DetalleVentaInterface>(endpoint,'GET');
   }
   addDetalleVenta(dv:AddDetalleVenta[]){
-    return this.http.post(`${this.baseUrl}/dv/create${this.token}`,dv)
+    const endpoint=`/dv/create`
+    return this.checkUrls<DetalleVentaInterface>(endpoint,'POST', dv)
   }
   deleteDetalleVenta(id:number){
-    return this.http.delete(`${this.baseUrl}/dv/delete/${id}${this.token}`)
+    const endpoint=`/dv/delete/${id}`
+    return this.checkUrls<DetalleVentaInterface>(endpoint,'DELETE')
   }
   /*-------------------------------------------------------------------------------------*/
   /*---------------------------------------VENTAS----------------------------------------*/
 
   /*-------------------------------------------------------------------------------------*/
   getVentas(){
-    return this.http.get<VentaInterface>(`${this.baseUrl}/venta${this.token}`)
+    const endpoint = `/venta`;
+    return this.checkUrls<VentaInterface>(endpoint,'GET');
   }
   getVentasByClave(clave:string){
-    return this.http.get<VentaInterface>(`${this.baseUrl}/venta/ver-ventas/${clave}${this.token}`)
+    const endpoint = `/venta/ver-ventas/${clave}`;
+    return this.checkUrls<VentaInterface>(endpoint,'GET');
   }
   createVenta(folio:string,clave:string){
-    return this.http.post<VentaInterface>(`${this.baseUrl}/venta/create${this.token}`,
+    const endpoint=`/venta/create`
+    return this.checkUrls<VentaInterface>(endpoint,'POST',
       new AddVenta(folio,0,clave,1))
   }
   deleteVenta(folio:string){
-    return this.http.put<VentaInterface>(`${this.baseUrl}/venta/delete/${folio}${this.token}`,'')
+    const endpoint=`/venta/delete/${folio}`
+    return this.checkUrls<VentaInterface>(endpoint,'PUT','')
   }
 }
